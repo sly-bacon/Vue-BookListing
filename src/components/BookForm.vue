5f7a7ef645b0fad8ab61e76a657405bc43c2e948
Eric Cunningham <ericgcunningham@hotmail.com>
2019-03-26 12:48:43 -0500

    Cleared form fields and put focus on book title after submit

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,29 +1,38 @@
 <template>
-    <form class="" action="#" method="post" v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">
-        <input type="text" name="title" value="" placeholder="Book Title" v-model="bookTitle">
-        <input type="text" name="author" value="" placeholder="Book Author" v-model="bookAuthor">
-        <button type="submit" name="button">Add Book</button>
-    </form>
+  <form class action="#" method="post" v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">
+    <input
+      type="text"
+      name="title"
+      value
+      placeholder="Book Title"
+      ref="bookTitle"
+      v-model="bookTitle"
+    >
+    <input type="text" name="author" value placeholder="Book Author" v-model="bookAuthor">
+    <button type="submit" name="button">Add Book</button>
+  </form>
 </template>
 
 <script>
 export default {
-    name: 'BookForm',
-    props: ['books'],
-    data() {
-        return {
-            bookTitle: '',
-            bookAuthor: ''
-        }
-    },
-    methods: {
-        bookSubmit(bookTitle, bookAuthor){
-            this.$emit('addBook', bookTitle, bookAuthor);
-        }
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+      bookTitle: "",
+      bookAuthor: ""
+    };
+  },
+  methods: {
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit("addBook", bookTitle, bookAuthor);
+      this.bookTitle = "";
+      this.bookAuthor = "";
+      this.$refs.bookTitle.focus();
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
